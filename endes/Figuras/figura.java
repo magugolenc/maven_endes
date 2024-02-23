c23647c90da516deb62903c3a1b81f0ce3083b49
JoaquinG <jogagu2005@gmail.com>
2024-02-23 12:50:42 +0100

    Organizacion carpetas

---
+++ endes/Figuras/figura.java
@@ -0,0 +1,14 @@
+@author Joaquin
+
+public abstract class figura {
+	/**
+	Retorna el area
+	@return area como double
+	*/
+	public abstract double area();
+	/**
+	Retorna el perimetro
+	@return perimetro como double
+	*/
+	public abstract double perimetro();
+}
