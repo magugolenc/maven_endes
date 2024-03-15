5b0d161dd015cce99fa2deb1d3a975298bc89944
magugolenc <jogagu2005@gmail.com>
2024-03-15 14:06:47 +0100

    Examen endes

---
+++ Examen/src/main/java/com/prueba/endes/App.java
@@ -0,0 +1,16 @@
+package com.prueba.endes;
+
+import com.github.javafaker.Faker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Faker f =new Faker();
+        System.out.println( f.gameOfThrones());
+    }
+}
